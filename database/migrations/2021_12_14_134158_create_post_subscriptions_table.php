bf7d6899b7e9da19dacc2afcbb02da41e977d484
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-20 00:17:44 +0900

    ポートフォリオ修正後

--- database/migrations/2021_12_14_134158_create_post_subscriptions_table.php
+++ database/migrations/2021_12_14_134158_create_post_subscriptions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubscriptionsTable extends Migration
+class CreatePostSubscriptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSubscriptionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('subscriptions', function (Blueprint $table) {
+        Schema::create('post_subscriptions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('post_id')->unsigned();
@@ -31,6 +31,6 @@ class CreateSubscriptionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('post_subscriptions');
     }
 }
