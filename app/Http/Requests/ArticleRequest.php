d59172c17b053e42d5eadc16257d63f6a6e8dd9f
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-20 16:51:59 +0900

    Article2

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\ArticleRequest;
 
-class PostRequest extends FormRequest
+class PostRequest extends ArticleRequest
 {
     /**
      * Determine if the user is authorized to make this request.
