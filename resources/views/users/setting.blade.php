c8b9da1a3cde513618ebd99c92bba56daeff1c3d
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-21 20:15:43 +0900

    name

--- resources/views/users/setting.blade.php
+++ resources/views/users/setting.blade.php
@@ -5,6 +5,7 @@
   @method('put')
   @csrf
   <label>アイコン選択:  <input type="file"  name="icon" accept="icon/png, icon/jpeg, icon/jpg"></input></label>
+  <textarea name="name" placeholder="名前を変更しましょう"></textarea>
   <textarea name="profile" placeholder="自己紹介を書きましょう"></textarea>
   <button type=submit>追加</button>
  </form>
