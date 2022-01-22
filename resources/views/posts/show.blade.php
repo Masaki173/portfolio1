77961885359f772e55c6a958f126843c788d7f28
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-23 00:46:57 +0900

    is_null位置

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,9 +23,10 @@
 @elseif($post->category_code === 5)
 <a href="/{{$post->category_code}}" class="category-url">エンタメ</a>
 @endif
+
+@if($post->is_fee === 1 && !$post->is_subscribed_by_auth_user())
 @auth
-@if($post->user->id !== Auth::user()->id && $post->is_fee === 1 && !$post->is_subscribed_by_auth_user())
-@if(is_null(Auth::user()->stripe_code))
+@if(is_null(Auth::user()->stripe_code)　&& $post->user->id !== Auth::user()->id)
 <h2 class="restricted-content">{{Str::limit($post->content, 60,'...')}}</h2>
 <p class="limit-line">--------この投稿をお読みいただくには購読が必要です--------</p>
 <form>
