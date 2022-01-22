9fc387989a5623b7ee990b11f3f4e82c04305b4d
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-23 02:06:38 +0900

    if配置

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -24,7 +24,7 @@
 <a href="/{{$post->category_code}}" class="category-url">エンタメ</a>
 @endif
 @auth
-@if($post->user->id !== Auth::user()->id && $post->is_fee === 1 && !$post->is_subscribed_by_auth_user())
+@if(Auth::user()->id !== $post->user->id && $post->is_fee === 1 && !$post->is_subscribed_by_auth_user())
 @if(is_null(Auth::user()->stripe_code))
 <h2 class="restricted-content">{{Str::limit($post->content, 60,'...')}}</h2>
 <p class="limit-line">--------この投稿をお読みいただくには購読が必要です--------</p>
