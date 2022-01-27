6248a93c420f3511911020d926559f9e7109c23d
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-27 22:05:20 +0900

    getEmail

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -22,6 +22,7 @@ class SocialAccountController extends Controller
 
         $authUser = $accountService->findOrCreate(
             $user,
+            $user->getEmail(),
             $provider
         );
 
