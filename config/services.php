cda9e2ae7e0ef645465681377eadb1e261cd4d3e
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-27 21:50:42 +0900

    facebook

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'facebook' => [
         'client_id' =>  env('FACEBOOK_APP_ID'),
         'client_secret' => env('FACEBOOK_APP_SECRET'),
-        'redirect' =>  env('APP_URL'),
+        'redirect' =>  env('APP_URL') . '/login/facebook/callback',
     ],
     
     'twitter'=>[
