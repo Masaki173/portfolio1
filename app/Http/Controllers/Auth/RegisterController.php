9e14b9f69d4d111bf701441576e78dd86ec748a1
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-29 17:02:57 +0900

    icon

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-//             'icon' => 'storage/' . 'DefaultIcon.jpg'
+            'icon' => 'public/' . 'DefaultIcon.jpg'
             'password' => Hash::make($data['password']),
         ]);
     }
