b67f4c643e12b2ff0afc86dfc1722c802665d581
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-31 02:15:15 +0900

    試験

--- resources/views/posts/home.blade.php
+++ resources/views/posts/home.blade.php
@@ -27,7 +27,7 @@
 <table>
 <tr><th><a href="/posts/{{$item->id}}">{{$item->title}}</a></th></tr>
 @if($item->is_fee === 1){
-<tr><td><p>¥{{$item->price}}</p></td></tr>
+<tr><td><p>{{$item->price}}</p></td></tr>
 }
 <tr>
 <td>
