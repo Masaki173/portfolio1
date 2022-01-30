ebb19791642b56f101a87fdd94bc0c6db966d168
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-30 20:04:06 +0900

    delete title,content,is_fee

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -25,16 +25,4 @@
 </span>
   <button type=submit>追加</button>
  </form>
- @error('title')
- <tr><th>Error</th>
- <td>{{$message}}</td></tr>
- @enderror
- @error('content')
- <tr><th>Error</th>
- <td>{{$message}}</td></tr>
- @enderror
- @error('is_fee')
- <tr><th>Error</th>
- <td>{{$message}}</td></tr>
- @enderror
 @endsection
