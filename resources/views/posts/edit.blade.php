17e22920822d1adda62f3e865a8eb5385446a6ee
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-30 21:02:51 +0900

    delete is_fee

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -13,20 +13,6 @@
     <option value="5">エンタメ</option>
   </select>
     <label>
-  <input type="radio" class="radioFee"  name="is_fee" value="0"  onclick="priceSwitch()">
-無料
-</label>
-<label>
-  <input type="radio" class="radioFee"  name="is_fee" value="1" onclick="priceSwitch()">
-有料
-</label>
-<span id="pricing-form">
-<label>￥<input type="tel" minlength="3" name="price"></input> JPY</label>
-</span>
   <button type=submit>追加</button>
  </form>
-  @error('price')
- <tr><th>Error</th>
- <td>{{$message}}</td></tr>
- @enderror
 @endsection
