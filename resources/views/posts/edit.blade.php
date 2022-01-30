e375bd51eff5d52dcbb780d0a5d78be7fc27c434
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-30 18:46:52 +0900

    編集内容

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,10 +1,17 @@
 @extends('layouts.creations')
 @section('content')
-<form action="update/{id}" method ="post">
+<form action="update/{id}" method ="post" autocomplete="off">
   @method('put')
   @csrf
-  <input type="text"  name="title" value="{{$post->title}}"></input>
+   <input type="text"  name="title" value="{{$post->title}}"></input>
   <textarea name="content">{{$post->content}}</textarea>
+  <select name="category">
+    <option value="1">生活</option>
+    <option value="2">社会</option>
+    <option value="3">カルチャー</option>
+    <option value="4">テクノロジー</option>
+    <option value="5">エンタメ</option>
+  </select>
   <button type=submit>追加</button>
  </form>
 @endsection
