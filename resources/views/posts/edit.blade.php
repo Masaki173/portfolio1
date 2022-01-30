d749dcd57f277c1664ea17f9d2b383d70ef3ed5c
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-30 18:43:39 +0900

    編集内容

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,1 +1,10 @@
-
+@extends('layouts.creations')
+@section('content')
+<form action="update/{id}" method ="post">
+  @method('put')
+  @csrf
+  <input type="text"  name="title" value="{{$post->title}}"></input>
+  <textarea name="content">{{$post->content}}</textarea>
+  <button type=submit>追加</button>
+ </form>
+@endsection
