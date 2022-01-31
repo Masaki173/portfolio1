797ccce2fb5ac13fee1f1638744f945d296fa660
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-01 00:01:55 +0900

    radio

--- resources/js/article.js
+++ resources/js/article.js
@@ -1,9 +1,10 @@
 'use strict';
 console.log('radioFee');
- function priceSwitch() {
-    const formBox = document.getElementById('pricing-form');
+   const formBox = document.getElementById('pricing-form');
   const radioFee = document.getElementsByClassName('radioFee');
+       radioFee[0].checked;
       formBox.style.display = "none";
+ function priceSwitch() {
         radioFee[0].addEventListener('change', function (){
         formBox.style.display = "none";
       });
