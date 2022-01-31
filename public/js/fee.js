7cb66318c7090315f669cd1b14dceef2b2444faf
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-01 00:55:44 +0900

    default

--- public/js/fee.js
+++ public/js/fee.js
@@ -4,12 +4,9 @@ console.log('radioFee');
  function priceSwitch() {
     const formBox = document.getElementById('pricing-form');
   const radioFee = document.getElementsByClassName('radioFee');
-   radioFee[0].checked;
-  formBox.style.display = "none";
-        radioFee[0].addEventListener('change', function (){
+        if(radioFee[0].checked){
         formBox.style.display = "none";
-      });
-        radioFee[1].addEventListener('change', function (){
-        formBox.style.display = "";
-      });
+      }else if(radioFee[1].checked){
+        formBox.style.display = "block";
+      }
   }
