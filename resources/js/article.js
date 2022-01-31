cd56550bc1d7bbe51f6f5c28d3a97d9761710f7a
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-31 22:46:45 +0900

    削除

--- resources/js/article.js
+++ resources/js/article.js
@@ -8,12 +8,8 @@ console.log('radioFee');
       });
         radioFee[1].addEventListener('change', function (){
         formBox.style.display = "block";
-          var priceForm = document.getElementById("priceForm");
-  　　　　　var form_value = priceForm.value;
-  　　if (form_value.match(/[0-9]+/g) != form_value ) {
-  	document.getElementById('alert').innerHTML = '数値でお値段を入力してください';
-    priceForm.value = '';
-  }
       });
+   
+    
   }
 
