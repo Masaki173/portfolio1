e754c79d1e15232549d500910002771db185fdbf
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-01 00:53:59 +0900

    default

--- public/js/fee.js
+++ public/js/fee.js
@@ -1,12 +1,11 @@
 'use strict';
 console.log('radioFee');
-  const radioFee = document.getElementsByClassName('radioFee');
-  const formBox = document.getElementById('pricing-form');
-　radioFee[0].checked;
-  formBox.style.display = "none";
+
  function priceSwitch() {
-//     const formBox = document.getElementById('pricing-form');
-//   const radioFee = document.getElementsByClassName('radioFee');
+    const formBox = document.getElementById('pricing-form');
+  const radioFee = document.getElementsByClassName('radioFee');
+   radioFee[0].checked;
+  formBox.style.display = "none";
         radioFee[0].addEventListener('change', function (){
         formBox.style.display = "none";
       });
