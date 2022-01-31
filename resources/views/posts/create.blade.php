4971ad457ef732b6f2a5b44540723f388f2a0791
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-31 20:47:09 +0900

    tr td

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,19 +3,19 @@
 <div class="create-form">
 <form action="store" method ="post" autocomplete="off" class="create-content">
   @csrf
-  <table class="create-post">
-  <tr><th><input type="text"  name="title" style="height:35px; width:500px; font-size: 60%;"></input></th>
-    <tr><td>
+  <table class="create-post"> 
+  <tr><td>
    @error('title')
  <tr><td>Error:{{$message}}</td></tr>
  @enderror
  </td></tr>
-  <tr><td><textarea name="content" style="height:250px; width:500px;"></textarea></td></tr>
-      <tr style="margin-bottom:10px;"><td>
+  <tr><th><input type="text"  name="title" style="height:35px; width:500px; font-size: 60%;"></input></th>
+   <tr><td>
    @error('content')
  <tr><td>Error:{{$message}}</td></tr>
  @enderror
  </td></tr>
+  <tr><td><textarea name="content" style="height:250px; width:500px;"></textarea></td></tr>
   <tr><td><select name="category">
     <option value="1">生活</option>
     <option value="2">社会</option>
@@ -25,6 +25,11 @@
   </select>
   </td></tr>
   <tr><td>
+ @error('is_fee')
+ <tr><td>Error:{{$message}}</td></tr>
+ @enderror
+</td></tr>
+  <tr><td>
   <label>
   <input type="radio" class="radioFee"  name="is_fee" value="0"  onclick="priceSwitch()">
 無料
@@ -33,14 +38,8 @@
   <input type="radio" class="radioFee"  name="is_fee" value="1" onclick="priceSwitch()">
 有料
 </label>
-
 <span id="pricing-form">
 <label>￥<input type="tel" minlength="3" name="price" style="height:30px; font-size: 60%;"></input> JPY</label>
-</td></tr>
- <tr><td>
- @error('is_fee')
- <tr><td>Error:{{$message}}</td></tr>
- @enderror
 </td></tr>
 <tr><td>
 </span>
