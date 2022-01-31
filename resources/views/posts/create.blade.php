0b9111b919da83ac1ed2d7c5dd7da972527b9228
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-31 20:33:55 +0900

    error

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,15 +5,19 @@
   @csrf
   <table class="create-post">
   <tr><th><input type="text"  name="title" style="height:35px; width:500px; font-size: 60%;"></input></th>
+    <tr><td>
    @error('title')
  <tr><th>Error</th>
  <td>{{$message}}</td></tr>
  @enderror
+ </td></tr>
   <tr><td><textarea name="content" style="height:250px; width:500px;"></textarea></td></tr>
+      <tr><td>
    @error('content')
  <tr><th>Error</th>
  <td>{{$message}}</td></tr>
  @enderror
+ </td></tr>
   <tr><td><select name="category">
     <option value="1">生活</option>
     <option value="2">社会</option>
@@ -31,13 +35,16 @@
   <input type="radio" class="radioFee"  name="is_fee" value="1" onclick="priceSwitch()">
 有料
 </label>
+
 <span id="pricing-form">
 <label>￥<input type="tel" minlength="3" name="price" style="height:30px; font-size: 60%;"></input> JPY</label>
 </td></tr>
+ <tr><td>
  @error('is_fee')
  <tr><th>Error</th>
  <td>{{$message}}</td></tr>
  @enderror
+</td></tr>
 <tr><td>
 </span>
   <button type=submit>追加</button>
