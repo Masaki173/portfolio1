a21411d2460564ab45dfe322524a622452b8d9e1
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-31 21:56:20 +0900

    getElement

--- resources/js/article.js
+++ resources/js/article.js
@@ -2,6 +2,7 @@
 console.log('radioFee');
  function priceSwitch() {
     const formBox = document.getElementById('pricing-form');
+    
   const radioFee = document.getElementsByClassName('radioFee');
         radioFee[0].addEventListener('change', function (){
         formBox.style.display = "none";
