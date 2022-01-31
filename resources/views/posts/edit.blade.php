a2ee850d21a71586aa4fde2c72298ee96b8935f1
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-01-31 20:22:51 +0900

    table位置

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.creations')
 @section('content')
-<table>
+
 <form action="/posts/update/{{$post->id}}" method ="post" autocomplete="off">
   @method('put')
   @csrf
+  <table class="edit-post">
    <input type="text"  name="title" value="{{$post->title}}" style="height:35px; width:500px; font-size: 60%;"></input>
   <textarea name="content" style="height:250px; width:500px; ">{{$post->content}}</textarea>
   <select name="category">
@@ -15,6 +16,6 @@
   </select>
     <label>
   <button type=submit>追加</button>
+   </table>
  </form>
- </table>
 @endsection
