9e460736958aac1ea1e2134116258bd71140ea1f
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-01 00:39:59 +0900

    if

--- resources/js/article.js
+++ resources/js/article.js
@@ -1,18 +1,14 @@
 'use strict';
 console.log('radioFee');
-  const formBox = document.getElementById('pricing-form');
-  const radioFee = document.getElementsByClassName('radioFee');
-　radioFee[0].checked;
-  formBox.style.display = "none";
+ 
  function priceSwitch() {
       const formBox = document.getElementById('pricing-form');
   　　const radioFee = document.getElementsByClassName('radioFee');
-        radioFee[0].addEventListener('change', function (){
+        if(radioFee[0].checked){
         formBox.style.display = "none";
-      });
-        radioFee[1].addEventListener('change', function (){
+      }else if(radioFee[1].checked){
         formBox.style.display = "block";
-      });
+      }
   }
 // {
 //     var priceForm = document.getElementById("priceForm");
