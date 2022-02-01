6b7aabb410efc7133ebc427681aede18151a3ae7
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-01 13:50:11 +0900

    /

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -13,9 +13,9 @@ use App\Models\Post;
 use App\Models\Payment;
 class PaymentController extends Controller
 {
-    public function getPaymentForm(Request $request, $id){
+    public function getPaymentForm(Request $request){
         $user = Auth::user();
-         $post = Post::with('user')->findorFail($id);
+//          $post = Post::with('user')->findorFail($id);
         return view('posts.payment.form', compact('user', 'post'));
 
     }
@@ -59,7 +59,7 @@ class PaymentController extends Controller
         }
 
 
-       return redirect(route('post.show', ['id' => $id,]))->with("success", "カード情報の登録が完了しました。");
+       return redirect('/')->with("success", "カード情報の登録が完了しました。");
     }
 
 
