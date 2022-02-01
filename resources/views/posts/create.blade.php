67f4238d33a4d9d32c4df70ffbf85c95fc19cc12
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-02 02:29:59 +0900

    div

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,27 +9,28 @@
  <tr><td>Error:{{$message}}</td></tr>
  @enderror
  </td></tr>
-<div class="input-title"><input type="text"  name="title" placeholder="記事のタイトルを書きましょう" style="height:35px; width:500px; font-size: 60%;"></input></div>
+<div class="input-title"><input type="text"  name="title" placeholder="記事のタイトルを書きましょう" style="height:35px; width:500px; font-size: 70%;"></input></div>
    <tr><td>
    @error('content')
  <tr><td>Error:{{$message}}</td></tr>
  @enderror
  </td></tr>
-  <tr><td><textarea name="content" placeholder="記事の内容を書きましょう" style="height:250px; width:500px;"></textarea></td></tr>
-  <tr><td><select name="category">
+<div class="input-content"><textarea name="content" placeholder="記事の内容を書きましょう" style="height:250px; width:500px;"></textarea></div>
+  <div class="select-category">
+  <select name="category">
     <option value="1">生活</option>
     <option value="2">社会</option>
     <option value="3">カルチャー</option>
     <option value="4">テクノロジー</option>
     <option value="5">エンタメ</option>
   </select>
-  </td></tr>
+  </div>
   <tr><td>
  @error('is_fee')
  <tr><td>Error:{{$message}}</td></tr>
  @enderror
 </td></tr>
-  <tr><td>
+  <div class="select-isfee">
   <label>
   <input type="radio" class="radioFee"  name="is_fee" value="0"  onclick="priceSwitch()" checked="checked">
 無料
@@ -40,11 +41,11 @@
 </label>
 <span id="pricing-form">
 <label>￥<input type="tel" id="priceForm" minlength="3" name="price" value="100" style="height:30px; font-size: 70%;" required></input> JPY</label>
-</td></tr>
-<tr><td>
 </span>
+</div>
+<div class="add-content">
   <button type=submit>追加</button>
- </td></tr>
+ </div>
  </form>
  </div>
  @endsection
