cafd5f18dc123c1f6492953e2e6a468b6698a34e
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-03 00:26:26 +0900

    delete mb4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,7 +52,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4 button_wrapper">
+                            <div class="col-md-8 button_wrapper">
                                 <button type="submit" class="btn btn-primary login-e">
                                     {{ __('Login') }}
                                 </button>
