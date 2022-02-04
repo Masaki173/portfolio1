8dc446c9412901587bf5d7064782c4534df56921
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-04 15:33:34 +0900

    btn

--- resources/views/users/setting.blade.php
+++ resources/views/users/setting.blade.php
@@ -7,6 +7,6 @@
   <label>アイコン選択:  <input type="file"  name="icon" accept="icon/png, icon/jpeg, icon/jpg"></input></label>
   <textarea name="name" placeholder="名前を変更しましょう"></textarea>
   <textarea name="profile" placeholder="自己紹介を書きましょう"></textarea>
-  <button type=submit>追加</button>
+  <button type=submit class="btn btn-primary">追加</button>
  </form>
  @endsection
