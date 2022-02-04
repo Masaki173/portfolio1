159726b6c4d25af7ab25ee16942886683a527c20
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-04 15:46:19 +0900

    add div class

--- resources/views/users/setting.blade.php
+++ resources/views/users/setting.blade.php
@@ -4,9 +4,9 @@
 <form action="update" method ="post" enctype="multipart/form-data" class="user-detail">
   @method('put')
   @csrf
-  <label>アイコン選択:  <input type="file"  name="icon" accept="icon/png, icon/jpeg, icon/jpg"></input></label>
-  <textarea name="name" placeholder="名前を変更しましょう"></textarea>
-  <textarea name="profile" placeholder="自己紹介を書きましょう"></textarea>
+  <div class="select-icon"><label>アイコン選択:  <input type="file"  name="icon" accept="icon/png, icon/jpeg, icon/jpg"></input></label></div>
+  <div class="change-name"><input type="text" name="name" placeholder="名前を変更しましょう"></textarea></div>
+  <div class="write-profile"><textarea name="profile" placeholder="自己紹介を書きましょう"></textarea></div>
   <button type=submit class="btn btn-primary">追加</button>
  </form>
  @endsection
