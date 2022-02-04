bc60b3b21b1fd48e05b79dc378cb059730e2c8f7
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-04 15:31:52 +0900

    }}

--- resources/views/users/follows.blade.php
+++ resources/views/users/follows.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 @foreach ($follows as $follow_user)
 <table>
-<tr><th><a href="users/{{$follow_user->id}}"><img src="{{ Storage::url($follow_user->icon)" class="rounded-circle" width="35px" height="35px">{{$follow_user->name}}</a></th></tr>
+<tr><th><a href="users/{{$follow_user->id}}"><img src="{{ Storage::url($follow_user->icon)}}" class="rounded-circle" width="35px" height="35px">{{$follow_user->name}}</a></th></tr>
 <tr><td><p>{{Str::limit($follow_user->profile, 60,'...')}}</p></td></tr>
 </table>
 @endforeach
