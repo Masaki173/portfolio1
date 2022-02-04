e66e3c71a14071a4f54161a5c94f1aa653010714
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-05 03:28:30 +0900

    delete @if

--- resources/views/posts/payment/tip.blade.php
+++ resources/views/posts/payment/tip.blade.php
@@ -7,23 +7,6 @@
             <div class="card">
                 <div class="card-header">{{$post->user->name}}さんの投稿、「{{ $post->title }}」にチップを投げる</div>
                 <div class="card-body">
-                @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @if (session('success'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-
-                    @if (session('errors'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ session('errors') }}
-                        </div>
-                    @endif
                     <div class="form-group">
                         <ul class="list-group">
                             <li class="list-group-item">
