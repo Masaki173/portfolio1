6dcde416011e38c0196cf2c65dc70af0043c0def
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-04 16:10:18 +0900

    delete class

--- resources/views/users/setting.blade.php
+++ resources/views/users/setting.blade.php
@@ -4,7 +4,7 @@
 <form action="update" method ="post" enctype="multipart/form-data" class="user-detail">
   @method('put')
   @csrf
-  <div class="select-icon"><label>アイコン選択:  <input type="file"  name="icon" class="icon-setting" accept="icon/png, icon/jpeg, icon/jpg"></input></label></div>
+  <div class="select-icon"><label>アイコン選択:  <input type="file"  name="icon" accept="icon/png, icon/jpeg, icon/jpg"></input></label></div>
   <div class="change-name"><input type="text" name="name" class="name-setting" placeholder="名前を変更しましょう"></textarea></div>
   <div class="write-profile"><textarea name="profile" class="profile-stting" placeholder="自己紹介を書きましょう"></textarea></div>
   <button type=submit class="btn btn-primary">追加</button>
