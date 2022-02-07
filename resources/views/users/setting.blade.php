888a7a8fc1d839b01ba2d40bdf53210af87895f1
Masaki173 <79630035+Masaki173@users.noreply.github.com>
2022-02-07 23:23:22 +0900

    変更

--- resources/views/users/setting.blade.php
+++ resources/views/users/setting.blade.php
@@ -7,6 +7,6 @@
   <div class="select-icon"><label>アイコン選択:  <input type="file"  name="icon" accept="icon/png, icon/jpeg, icon/jpg"></input></label></div>
   <div class="change-name"><input type="text" name="name" class="name-setting" placeholder="名前を変更しましょう"></textarea></div>
   <div class="write-profile"><textarea name="profile" class="profile-setting" placeholder="自己紹介を書きましょう"></textarea></div>
-  <button type=submit class="btn btn-primary">追加</button>
+  <button type=submit class="btn btn-primary">変更</button>
  </form>
  @endsection
